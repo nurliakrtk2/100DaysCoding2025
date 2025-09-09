7c8a7baeb439bde3ab7af337048ca0a3522d3d01
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-09 20:26:08 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,13 @@
+package Day2.java; 
+
+public class Main {
+
+public static void main(String[] args) {
+  String nama = "NurliaKartika";
+  int umur = 18; 
+  double tinggi = 150.0;
+  System.out.println("nama saya %n saya berumur %d dan tinggi saya itu %.1f \n" ,nama,umur,tinggi); 
+  System.out.println("nama : "+ nama);
+  System.out.println("umur "+ umur);
+}
+}
