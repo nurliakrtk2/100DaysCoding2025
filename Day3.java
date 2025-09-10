669d0feb3cf0a5dd31b7a1ed5bcb9e206302bfd9
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-10 19:09:39 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,22 @@
+package Day3; 
+
+public class Day3 {
+
+public static void main(String[] args) {
+  System.out.println("Nurlia Kartika");
+  //2 jenis komentar yaitu:
+  /*Komentar satu baris dan
+    * Komentar multi-baris 
+    */
+
+  //Komentar satu baris: Diawali dengan dua garis miring dan berlaku hingga akhir baris.
+  //ini adalah komentar satu baris 
+
+  /*Komentar multi-baris: Diawali dan diakhiri dengan garis miring dan bintang.
+    /* Ini adalah komentar multi-baris
+    * dan seperti ini jika di enter 
+    */ 
+//
+}
+
+} 
