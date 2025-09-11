f405633232b638d524ae094ebb6bbb4561b22601
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-11 20:33:18 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,25 @@
+package day4.java;
+
+pucblic class main {
+
+pucblic static void main(String[] args) {
+
+  System.out.println("Hello\nWord");
+  System.out.println("Halo perkenalkan nama saya \nNurlia Kartika \nBiasa dipanggil liaaa");
+  System.out.println("My Birthday\t14 Mei")
+  System.out.println("Sekarang saya berusia\t18 tahun");
+  System.out.println("Saat ini saya berkuliah di \"Universitas Sulawesi Barat\"");
+  System.out.println("Saya mengambil prodi \'Informatika\' dengan NIM \'D0225307\'");
+  System.out.println("Hello \bWord");
+
+  // Beberapa fungsi pengguna Escape Sequence
+  //\n = new line (baris baru) digunakan untuk baris baru
+  //\t = tab (indentasi) digunakan untuk membuat jarak atau spasi tabulasi
+  //\b = backspase (menghapus karakter sebelumnya) digunakan untuk menghapus karakter atau spasi
+  //\ = backslash (mewakili karakter backslash) digunakan untuk menambahkan garis miring
+  // ' = single qoute (mewakili karakter single qoute) digunakan untuk menambahkan petik satu
+  // " = double qoute (mewakili karakter double qoute) digunakan untuk menambahkan petik dua
+
+}
+
+}
