457edc252461f8368e7cddd971c913e7999e96aa
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-12 21:31:32 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,27 @@
+package day5;
+
+public class Main; {
+
+public static void main(String[] args) {
+  byte saudara = 4;
+  short anak = 2;
+  long my birthday = 14;
+  int umur = 18;
+
+  //untuk menampilkan (output)
+  System.out.println("saya mempunyai saudara"+ saya);
+  System.out.println("saya anak ke"+ saya);
+  System.out.peintln("tanggal lahir saya"+ saya);
+  System out.println("umur saya"+ umur);
+
+  
+  //penjelasan mengenai tipe data yang digunakan
+  /*byte :tipe data dengan jangkauan yang lebih besar dari byte.
+  /*short :tipe dengan jangkauan yang lebih besar dari byte.
+  /*long : tipe data untuk nilai yang melebihi kapasitas jangkauan tipe data int,
+  *sehingga memiliki rentang yang jauh lebih tinggi.b
+  /*int : tipe data bilangan bulat yang paling umum
+
+    }
+
+}
