50c3a5c3976544f10e7820b2c45986e3ed36d583
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-12 22:37:36 +0800

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -5,22 +5,23 @@ public class Main; {
 public static void main(String[] args) {
   byte saudara = 4;
   short anak = 2;
-  long my birthday = 14;
+  long tanggal = 14;
   int umur = 18;
 
   //untuk menampilkan (output)
-  System.out.println("saya mempunyai saudara"+ saya);
-  System.out.println("saya anak ke"+ saya);
-  System.out.peintln("tanggal lahir saya"+ saya);
+  System.out.println("saya mempunyai saudara"+ saudara);
+  System.out.println("saya anak ke"+ anak);
+  System.out.peintln("tanggal lahir saya"+ tanggal);
   System out.println("umur saya"+ umur);
 
   
-  //penjelasan mengenai tipe data yang digunakan
-  /*byte :tipe data dengan jangkauan yang lebih besar dari byte.
-  /*short :tipe dengan jangkauan yang lebih besar dari byte.
-  /*long : tipe data untuk nilai yang melebihi kapasitas jangkauan tipe data int,
-  *sehingga memiliki rentang yang jauh lebih tinggi.b
-  /*int : tipe data bilangan bulat yang paling umum
+  //penjelasan dan perbedaan dari 4 tipe data tersebut:
+  //byte, short, int, dan long adalah tipe data untuk bilangan bulat, yang membedakan itu adalah ukuran memori dan jangkauan nilai yg dapat mereka tampung (simpan).
+  //byte : ukuran 8 bit, jangkauan kecil (-128 hingga 127).
+  //short : ukuran 16 bit, jangkauan lebih besar dari byte.
+  //int : ukuran 32 bit, standar untuk bilangan bulat.
+  //long : ukuran 64 bit, jangkauan terbesar, untuk nilai yang sangat besar.
+  
 
     }
 
