2a8854ff66398a3e3d23fc53802d51627a75da82
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-13 22:29:41 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,17 @@
+package Day6;
+
+public class main; {
+  public static void main(String[] args) {
+    float floatNumber = 3.14f;
+    double doubleNumber = 3.14159;
+
+  System.out.println("FloatNumber");
+  System.out.println("DoubleNumber");
+
+  //penjelasan dari tipe data float dan juga double 
+  //*float : tipe data float umumnya cukup untuk menyimpan sekitar 7 digit desimal secara akurat.
+  //*double : tipe data double umumnya cukup intuk menyimpan sekitar 15-16 digit desimal secara akurat.
+
+  }
+
+}
