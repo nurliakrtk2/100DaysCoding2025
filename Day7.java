7b963d4da361f5834914284740287eff1393b38a
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-14 12:48:33 +0800

    Update Day7.java

--- Day7.java
+++ Day7.java
@@ -6,6 +6,9 @@ public class main {
     char karakter = 'A';
     boolean bool = true; 
 
+    System.out.println("Karakter:" + karakter);
+    System.out.println("Boolean:" + bool); 
+
     //penjelasan dari tipe data char dan boolean 
     //*char : tipe data char itu hanya bisa menyimpan satu karakter dan memakai tanda kutip satu (')
     //*boolean : tipe data boolean hanya dapat menyimpan dua nilai yaitu true (benar) dan false (salah)
