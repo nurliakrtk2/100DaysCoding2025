f46eea749f58f64ec09a9964eef8fa8a109a87b5
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-14 11:48:11 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,16 @@
+package Day7;
+
+public class main {
+
+  public static void main(String[] args) {
+    char karakter = 'A';
+    boolean bool = true; 
+
+    //penjelasan dari tipe data char dan boolean 
+    //*char : tipe data char itu hanya bisa menyimpan satu karakter dan memakai tanda kutip satu (')
+    //*boolean : tipe data boolean hanya dapat menyimpan dua nilai yaitu true (benar) dan false (salah)
+
+  }
+
+}
+
