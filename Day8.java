7f40ced8da5bb9cf127310459e2e37a318e5a7f7
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-15 18:57:51 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,23 @@
+public class Day8;
+
+public class main {
+   public static void main(String[] args) ;
+      String nama = "Nurlia Kartika";
+      String alamat = "Saya dari tutar";
+      String unsulbar = "Perguruan Tinggi Negeri Majene";
+      String informatika = "salah satu jurusan dengan banyak peminat di unsulbar";
+      String tentangIT = "didalam informatika kita belajar coding";
+      String coding = "menulis petunjuk supaya komputer mengerti dan melakukan sesuatu yang kita mau";
+
+      System.out.println("nama" + nama);
+      System.out.println("alamat" + alamat);
+      System.out.println("unsulbar" + unsulbar);
+      System.out.println("informatika" + informatika);
+      System.out.println("coding" + coding);
+
+      //*String : untuk menyimpan teks atau sekumpulan karakter 
+      //*String : di tulis untuk tanda petik ganda (") 
+
+   }
+
+}
