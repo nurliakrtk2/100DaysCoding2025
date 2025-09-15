09ad1560ec823625662b508e7aacc6e436ad904a
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-15 19:35:52 +0800

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -9,9 +9,9 @@ public class main {
       
       
       System.out.println("Annyeonghaseyo nama saya lia" + nama);
-      System.out.println("alamat saya" + alamat);
+      System.out.println("alamat saya tutar" + alamat);
       System.out.println("saya berkuliah di Unsulbar" + kuliah;
-      System.out.println("di Informatika kita bekajar ngoding " + prodi );
+      System.out.println("di Informatika kita bekajar ngoding " + prodi);
    
 
       //penjelasan mengenai tipe data string
