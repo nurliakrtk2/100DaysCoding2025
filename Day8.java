d1a4a849d2a1db9ecd4e820b51be35752ca954bc
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-15 19:46:11 +0800

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -1,23 +1,24 @@
 public class Day8;
 
 public class main {
-   public static void main(String[] args) ;
+   
+   public static void main(String[] args) {
       String nama = "lia";
       String alamat = "tutar";
       String kuliah = "Unsulbar";
       String prodi = "informatika";
       
       
-      System.out.println("Annyeonghaseyo nama saya lia" + nama);
-      System.out.println("alamat saya tutar" + alamat);
-      System.out.println("saya berkuliah di Unsulbar" + kuliah;
-      System.out.println("di Informatika kita bekajar ngoding " + prodi);
+      System.out.println("Annyeonghaseyo nama saya" + nama);
+      System.out.println("alamat saya" + alamat);
+      System.out.println("saya berkuliah" + kuliah);
+      System.out.println("di Informatika kita bekajar ngoding" + prodi);
    
 
       //penjelasan mengenai tipe data string
       //*String : untuk menyimpan teks atau sekumpulan karakter 
       //*String : di tulis untuk tanda petik ganda (") 
 
-   }
+}
 
 }
