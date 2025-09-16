c929e3ef9c13d32ca6d422f5e3bbdbc8eac57f80
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-16 21:11:25 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,35 @@
+package Day9;
+
+public class main {
+
+public static void main(String[] args) { 
+  //deklarasi 
+  int tb;
+  float bb;
+
+  //inisialisasi 
+  tb = 150;
+  bb = 58;
+
+  //menampilkan nilai awal 
+  System.out.println("Tinggi badan tahun lalu : "+tb);
+  System.out.println("Berat badan tahun lalu : "+bb);
+
+  //update variabel 
+  tb = 152;
+  bb = 56.5f;
+
+  //menampilkan nilai setelah update 
+  System.out.println("Tinggi badan tahun lalu : "+tb);
+  System.out.println("Tinggi badan tahun lalu : "+bb);
+
+  /*
+  //*Deklarasi : menentukan tipe data dan nama variabel 
+  //*Inisialisasi : proses memberikan atau memasukkan nilai awal ke variabel setelah dideklarasikan 
+  //*Update variabel : mengubah nilai variabel setelah diinisialisasi 
+  /*
+
+    }
+
+    }
+  
