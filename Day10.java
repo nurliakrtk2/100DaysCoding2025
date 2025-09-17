ee200e896f2426afbf64a71068c747324e65d035
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-17 19:17:26 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,15 @@
+package Day10;
+
+public class main {
+
+publis static void main(String[] args) {
+  //gunakan final variabel untuk membuat nilai konstanta 
+  final int umur = 19;
+  System.out.println("umur saya : "+ umur);
+
+  //*final variabel : variabel yang nilainya tidak dapat diubah setelah diinisialisasi 
+  //*jika nilai konstanta diubah maka akan error 
+
+}
+
+}
