31054ca575fef0a20b37456fd669911d07ab0448
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-19 19:44:42 +0800

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,60 @@
+package day12.java;
+
+public class main {
+  public static void main(String[] args) {
+
+  //Deklarasi 
+  String nama, nim, alamat;
+  char jenisKelamin;
+  byte jumlahSaudara;
+  short tahunLahir;
+  int umur, tb, bb;
+  float nilaiRaport;
+  double lebar;
+  boolean sudahLulus;
+
+  //Inisialisasi 
+  nama = "Lia";
+  nim = "D0225307";
+  alamat = "Tutar";
+  jenisKelamin = 'P';
+  tahunLahir = 2006;
+  umur = 19;
+  tb = 150;
+  bb = 58;
+  noHp = 83166030775L;
+  nilaiRaport = 89.5f;
+  lebar = 15.5;
+
+  //output 
+  System.out.println("Nama : "+ nama);
+  System.out.println("NIM : "+ nim);
+  System.out.println("Jenis kelamin : "+ jenisKelamin);
+  System.out.println("Jumlah saudara : "+ jumlahSaudara);
+  System.out.println("Tahun Lahir : "+ tahunLahir);
+  System.out.println("Umur : "+ umur)
+  System.out.println("Tb : "+ tb);
+  System.out.println("Bb : "+ bb);
+  System.out.println("No Hp : "+ noHp);
+  System.out.peintln("Nilai Raport : "+ nilaiRaport);
+  System.out.println("Lebar : "+ lebar);
+  System.out.println("Sudah Lulus : "+ sudahLulus); 
+
+  /*
+  //tipe data yang saya gunakan dalam membuat biodata ini :
+  //String, char, byte, short, int, long, float, double, dan boolean (true/false)
+  //String untuk nama, nim, dan alamat
+  //char untuk jenis kelamin 
+  //byte untuk jumlah saudara 
+  //short untuk tahun lahir 
+  //int intuk umur, tb, dan bb
+  //long untuk no hp
+  //float untuk nilai raport
+  //double untuk lebar 
+  //boolean untuk sudah lulus
+  /*
+
+    }
+
+    }
+  
