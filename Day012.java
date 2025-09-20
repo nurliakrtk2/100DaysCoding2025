fbc8964f2ffef1635a1763bd628e509e1ef093b4
nurliakrtk2 <nurliakrtk@gmail.com>
2025-09-20 09:02:01 +0800

    Update and rename Day12.java to Day012.java

--- Day012.java
+++ Day012.java
@@ -1,33 +1,48 @@
 package day12.java;
 
+import java.util.Scanner;
+
 public class main {
   public static void main(String[] args) {
-
-//Deklarasi 
+  Scanner input = new Scanner(System.in);
+  //Deklarasi 
   String nama, nim, alamat;
   char jenisKelamin;
   byte jumlahSaudara;
   short tahunLahir;
   int umur, tb, bb;
+  long noHp;
   float nilaiRaport;
   double lebar;
   boolean sudahLulus;
-  long noHp;
 
   //Inisialisasi 
-  nama = "Lia";
-  nim = "D0225307";
-  alamat = "Tutar";
-  jenisKelamin = 'P';
-  tahunLahir = 2006;
-  umur = 19;
-  tb = 150;
-  bb = 58;
-  noHp = 83166030775L;
-  nilaiRaport = 89.5f;
-  lebar = 15.5;
-  sudahLulus = true;
-  jumlahSaudara = 4;
+  System.out.print("Nama : ");
+  nama = input.nextLine();
+  System.out.print(Nim :");
+  nim = input.nextLine();
+  System.out.print("Alamat :");
+  alamat = input.nextLine();
+  System.out.print("Jenis kelamin :"); 
+  jenisKelamin = input.next().charAt(0);
+  System.out.print("Jumlah Saudara :");
+  jumlahSaudara = input.nextByte();
+  System.out.print("Tahun lahir :");
+  tahunLahir = input.nextShort();
+  System.out.print("Umur :");
+  umur = input.nextInt();
+  System.out.peint("Tb :");
+  tb = input.nextInt();
+  System.out.print("Bb :");
+  bb = input.nextInt();
+  System.out.print("No Hp :");
+  noHp = input.nextLong();
+  System.out.print("Nilai Raport :");
+  nilaiRaport = input.nextFloat();
+  System.out.print("Lebar :");
+  lebar = input.nextDouble();
+  System.out.print("Sudah Lulus :");
+  sudahLulus = input.nextBoolean();
 
   //output 
   System.out.println("Nama : "+ nama);
@@ -44,7 +59,7 @@ public class main {
   System.out.println("Sudah Lulus : "+ sudahLulus);
     
   /*
-  //tipe data yang saya gunakan dalam membuat biodata ini :
+  //tipe data yang saya gunakan dalam membuat biodata Scanner ini :
   //String, char, byte, short, int, long, float, double, dan boolean (true/false)
   //String untuk nama, nim, dan alamat
   //char untuk jenis kelamin 
